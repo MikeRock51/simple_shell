1ed7177a4804d58cd0b647956e2af032887c19cc
MikeRock51 <mikerockmusic51@gmail.com>
2022-11-13 17:47:10 +0000

    Working shell

--- mesh.h
+++ mesh.h
@@ -5,8 +5,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 char *_getline();
 char **split_line(char *line);
+void execute(char **path);
 
 #endif
